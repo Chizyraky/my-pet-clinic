9aea553f9c085913c91b983988ff435f6e600263
Chizyraky <55993763+Chizyraky@users.noreply.github.com>
2021-03-16 17:43:32 +0200

    Initial Project Add

---
+++ src/main/java/chiz/springframework/chizpetclinic/ChizPetClinicApplication.java
@@ -0,0 +1,13 @@
+package chiz.springframework.chizpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChizPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChizPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chiz/springframework/chizpetclinic/ChizPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package chiz.springframework.chizpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChizPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
