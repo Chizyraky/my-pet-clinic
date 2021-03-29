67c6a349c3fae1836bbe0df173432328f03c9a08
Chizy Raky <chizyraky@gmail.com>
2021-03-29 20:12:21 +0300

    refactoring to multimodel maven project. closes #1

--- pet-clinic-web/src/test/java/chiz/springframework/chizpetclinic/ChizPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/chiz/springframework/chizpetclinic/ChizPetClinicApplicationTests.java
@@ -1,6 +1,6 @@
 package chiz.springframework.chizpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
