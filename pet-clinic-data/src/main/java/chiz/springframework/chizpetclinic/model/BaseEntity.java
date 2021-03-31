fddd11dbb8fe6d8394841ae45311e4182ed1b0de
Chizy Raky <chizyraky@gmail.com>
2021-03-31 17:10:48 +0300

    adding base entity. closes #10

---
+++ pet-clinic-data/src/main/java/chiz/springframework/chizpetclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package chiz.springframework.chizpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/chiz/springframework/chizpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/chiz/springframework/chizpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package chiz.springframework.chizpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet{
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
