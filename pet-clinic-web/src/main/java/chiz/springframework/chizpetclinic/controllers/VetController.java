dc09db06ecb6d6b922f86cd652dc0f16176fe8e5
Chizy Raky <chizyraky@gmail.com>
2021-04-07 18:56:47 +0300

    Adding vets page and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/chiz/springframework/chizpetclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package chiz.springframework.chizpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
