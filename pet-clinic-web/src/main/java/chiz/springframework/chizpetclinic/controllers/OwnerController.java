4d42a1e74bcf3c338666da8bfad01cc2f6afb696
Chizy Raky <chizyraky@gmail.com>
2021-04-07 19:56:47 +0300

    Adding Owners Index Page and Controller. closes #13

---
+++ pet-clinic-web/src/main/java/chiz/springframework/chizpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package chiz.springframework.chizpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
