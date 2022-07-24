a454b11ea924f64bf0af1f32d6b6aa447c072581
chizy <chizyraky@gmail.com>
2022-07-24 14:58:32 +0100

    Added Owners List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/chiz/springframework/chizpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/chiz/springframework/chizpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package chiz.springframework.chizpetclinic.controllers;
 
+import chiz.springframework.chizpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    public final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
